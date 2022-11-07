f48fee8f1f399be56a491c8ace1591a8a39af9d0
Imad Khan <kimad9963@gmail.com>
2022-11-07 14:51:27 +0500

    Add project files.

---
+++ TodoApi/Data/TodoApiContext.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using TodoApi.Models;
+
+    public class TodoApiContext : DbContext
+    {
+        public TodoApiContext (DbContextOptions<TodoApiContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<TodoApi.Models.TodoItem> TodoItem { get; set; } = default!;
+    }
